2f3555c0c8ea29353231df215bd0e42abe087a71
Steeve Droz <steeve.droz@rpn.ch>
2016-06-03 17:08:55 +0200

    Registration

---
+++ app/models/User.js
@@ -0,0 +1,16 @@
+var User = function(nick) {
+    this.nick = nick;
+    this.id = guid();
+    
+    function guid() {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
+}
+
+exports.User = User;
--- server.js
+++ server.js
@@ -2,10 +2,64 @@ var port = 8081;
 
 var app = require('express')();
 var http = require('http').Server(app);
+var User = require('./app/models/User').User;
+
+var users = [];
+
+// API
 
 app.get('/', function(req, res) {
-    res.send('Hello, World!');
+    res.sendFile(__dirname + '/app/api.html');
 });
+
+// Register
+
+app.post('/users/register/:nick', function(req, res) {
+    var availableNick = req.params.nick;
+    var nick = null;
+    var index = 0;
+    do {
+        var available = true;
+        for (var i in users) {
+            var user = users[i];
+            if (index == 0 && user.nick == availableNick){
+                available = false;
+                break;
+            }
+            if (user.nick == availableNick + '_' + index) {
+                available = false;
+                break;
+            }
+        }
+        if (available) {
+            nick = availableNick;
+            if (index > 0) {
+                nick += '_' + index;
+            }
+        }
+        else {
+            index++;
+        }
+    } while (nick == null);
+    
+    user = new User(nick);
+    users.push(user);
+    res.send(user);
+    console.log(users);
+    console.log();
+});
+
+// Error handling
+
+app.get('*', function(req, res) {
+   res.send({ 'error': 'Unknown route.' }); 
+});
+
+app.post('*', function(req, res) {
+   res.send({ 'error': 'Unknown route.' }); 
+});
+
+// Server listening
 
 http.listen(port, function() {
    console.log('Server started on port ' + port); 
