ce94980af35e04d4bfaf78d832ea0a757b774147
Steeve Droz <steeve.droz@rpn.ch>
2016-08-30 10:57:03 +0200

    Improved tests

--- spec/ChannelSpec.js
+++ spec/ChannelSpec.js
@@ -1,35 +1,69 @@
 var app = require('../server'),
     request = require('supertest');
 
-describe('App', function() {
-    it('join a channel', function(done) {
-        const agent = request.agent(app);
-
-        agent.post('/users/register/toto').end(function(err, res){
+describe('Valid app', function() {
+    const agent = request.agent(app);
+    let id;
+    it('connects to server', function(){
+        agent.post('/users/register/toto/').end(function(err, res) {
             expect(err).toBeNull();
-
-            const id = res.body.id;
+            id = res.body.id;
             expect(id).not.toBeUndefined();
-
-            agent.put('/user/' + id + '/channels/toto/join/').end(function(err, res) {
+        })
+    })
+    
+    it('joins a channel', function() {
+        agent.put('/user/' + id + '/channels/channel-1/join/')
+            .end(function(err, res) {
+                done()
                 expect(err).toBeNull();
-
-                const channels = res.body.channels;
-                expect(Object.keys(channels)).toContain('toto');
-
-                agent.put('/user/' + id + '/channels/toto/say/')
-                    .send({'message': 'hello world!'})
-                    .end(function(err, res) {
-                        expect(err).toBeNull();
-
-                        const notices = res.body.notices;
-                        expect(notices.length).toEqual(2);
-                        expect(notices[0].message).toBeUndefined();
-                        expect(notices[1].message).toEqual('hello world!');
-                        done();
-                    })
+                const user = res.body.user;
+                expect(user).not.toBeNull();
+                expect(user.id).toEqual(id);
             })
-
-        });
+    })
+    
+    it('talks in channel', function() {
+        agent.put('/user/' + id + '/channels/channel-1/say/')
+            .send({ message: 'Hello, world!' })
+            .end(function(err, res) {
+                done()
+                expect(err).toBeNull()
+                const user = res.body.user;
+                expect(user).not.toBeNull();
+                expect(user.id).toEqual(id);
+            })
+    })
+    
+    it('checks for notices', function() {
+        agent.put('/user/' + id + '/notices/')
+            .end(function(err, res) {
+                done()
+                expect(err).toBeNull()
+                const notices = res.body.notices
+                expect(notices).not.toBeNull()
+                expect(notices.length).toEqual(2)
+                expect(notices[0]['type']).toEqual('channelJoin');
+                expect(notices[0]['nick']).toEqual('toto');
+                expect(notices[0]['channel']).toEqual('channel-1');
+                
+                expect(notices[1]['type']).toEqual('channelMessage');
+                expect(notices[1]['nick']).toEqual('toto');
+                expect(notices[1]['channel']).toEqual('channel-1');
+            })
+    })
+    
+    it('disconnects', function() {
+        agent.put('/user/' + id + '/disconnect/')
+            .end(function(err, res) {
+                done()
+                expect(err).toBeNull()
+                const version = res.body.version
+                expect(version).not.toBeNull()
+            })
     })
 });
+
+describe('Invalid app', function() {
+    // TODO Invalid actions
+})
--- spec/UserSpec.js
+++ spec/UserSpec.js
@@ -1,11 +1,12 @@
 var User = require('../app/models/User')
 
 describe("User", function() {
-    it("has nick, id and channels", function() {
+    it("has nick, id, channels and notices", function() {
         let user = new User("Yoan")
         expect(user.nick).toEqual("Yoan")
         expect(user.id).not.toBeUndefined()
         expect(user.channels).not.toBeUndefined()
+        expect(user.notices).not.toBeUndefined()
     })
 
     it("must have a nick or be anonymous", function() {
