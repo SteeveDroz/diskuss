390c44697b5beda5481570b0169568ccaa80a992
Steeve Droz <steeve.droz@rpn.ch>
2016-09-01 14:14:34 +0200

    Added done parameter

--- spec/ChannelSpec.js
+++ spec/ChannelSpec.js
@@ -4,17 +4,18 @@ var app = require('../server'),
 describe('Valid app', function() {
     const agent = request.agent(app);
     let id;
-    it('connects to server', function(){
-        agent.post('/users/register/toto/').end(function(err, res) {
-			expect(true).toBe(false);
-            expect(err).toBeNull();
-            id = res.body.id;
-            expect(id).not.toBeUndefined();
-			done()
-        })
+    it('connects to server', function(done){
+        agent.post('/users/register/toto/')
+		.end(function(err, res) {
+				expect(true).toBe(false);
+				expect(err).toBeNull();
+				id = res.body.id;
+				expect(id).not.toBeUndefined();
+				done()
+			})
     })
     
-    it('joins a channel', function() {
+    it('joins a channel', function(done) {
         agent.put('/user/' + id + '/channels/channel-1/join/')
             .end(function(err, res) {
                 expect(err).toBeNull();
@@ -25,7 +26,7 @@ describe('Valid app', function() {
             })
     })
     
-    it('talks in channel', function() {
+    it('talks in channel', function(done) {
         agent.put('/user/' + id + '/channels/channel-1/say/')
             .send({ message: 'Hello, world!' })
             .end(function(err, res) {
@@ -37,7 +38,7 @@ describe('Valid app', function() {
             })
     })
     
-    it('checks for notices', function() {
+    it('checks for notices', function(done) {
         agent.put('/user/' + id + '/notices/')
             .end(function(err, res) {
                 expect(err).toBeNull()
@@ -55,7 +56,7 @@ describe('Valid app', function() {
             })
     })
     
-    it('disconnects', function() {
+    it('disconnects', function(done) {
         agent.put('/user/' + id + '/disconnect/')
             .end(function(err, res) {
                 expect(err).toBeNull()
