43c5d11ea0ecc9d65e18b40207332529ffef9840
Steeve Droz <steeve.droz@rpn.ch>
2016-09-09 12:17:15 +0200

    Added channel owner to server

--- server.js
+++ server.js
@@ -169,6 +169,34 @@ app.put('/user/:id/channels/:channel/keep', function(req, res) {
     res.send({ status: 'Changing the persistence', channel: channel })
 })
 
+// Give channel ownership
+
+app.put('/user/:id/channels/:channel/owner/:nick/', function(req, res) {
+    const user = store.getUser(req.params.id)
+    if (user === undefined) {
+        res.status(404).send({ error: 'Unknown user ID' })
+        return
+    }
+    const channel = store.getChannel(req.params.channel)
+    if (channel === undefined) {
+        res.status(404).send({ error: 'Unknown channel' })
+        return
+    }
+    const recipient = store.getUserByNick(req.params.nick)
+    if (recipient === undefined) {
+        res.status(404).send({ error: 'Unknown username' })
+        return
+    }
+    if (channel.owner !== user.nick) {
+        res.status(404).send({ error: 'You don\'t own the channel' })
+        return
+    }
+    channel.owner = recipient.nick
+    
+    notice({ type:'channelOwner', channel: channel, nick: user.nick })
+    res.send({ status: 'Ownership transfered', channel: channel })
+})
+
 // Leave channel
 
 app.delete('/user/:id/channels/:channel/leave/', function(req, res) {
@@ -242,6 +270,7 @@ function notice(message) {
         case 'channelLeave':
         case 'channelDescription':
         case 'channelKeep':
+        case 'channelOwner':
             const channel = store.getChannel(message.channel.name)
             if (channel !== undefined) {
                 const users = store.getUsersByChannel(channel.name, false)
